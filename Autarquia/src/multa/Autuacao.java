48a50205fdbdef8b3ecdc8795db5b33d52a75ec8
aluno <aluno@DESKTOP-3BUGJ3J>
2024-07-22 11:24:54 -0300

    Alterações feitas na segunda

--- Autarquia/src/multa/Autuacao.java
+++ Autarquia/src/multa/Autuacao.java
@@ -1,10 +1,19 @@
 package multa;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Autuacao {
-	List<Multa> multas;
-	multas = new ArrayList();
-	Multa m1 = new Multa();
-	multas.add(m1);
+	List<Multa> m;
+	Local local;
+	Veiculo veiculo;
+	
+	public Autuacao(Local local, Veiculo veiculo){
+		m = new ArrayList<Multa>();
+		
+	}
+	public void addMulta(Multa multa) {
+		m.add(multa);
+	}
+	
 }
